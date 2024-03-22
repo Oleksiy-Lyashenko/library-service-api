ba4a129ccb1a23e69102fbd8c99804b7e6b9cfd7
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2024-03-22 14:15:23 +0200

    add path borrowings to urls

--- service/urls.py
+++ service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from service.views import BookViewSet
+from service.views import BookViewSet, BorrowingViewSet
 
 router = routers.DefaultRouter()
 router.register("books", BookViewSet)
+router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls), )
